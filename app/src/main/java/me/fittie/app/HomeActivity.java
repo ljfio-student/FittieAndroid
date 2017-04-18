d8bb62d5afbce095ff8e898bea0253716cc7f46a
Luke Fisher <mail@lukefisher.me>
2017-04-18 22:24:15 +0100

    Change out FAB for one with 'speed dial'

--- app/src/main/java/me/fittie/app/HomeActivity.java
+++ app/src/main/java/me/fittie/app/HomeActivity.java
@@ -1,15 +1,15 @@
 package me.fittie.app;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import io.github.yavski.fabspeeddial.FabSpeedDial;
+import io.github.yavski.fabspeeddial.SimpleMenuListenerAdapter;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -31,12 +31,12 @@ public class HomeActivity extends AppCompatActivity {
         tabLayout.setupWithViewPager(viewPager);
 
         // Floating action button
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        FabSpeedDial fabSpeedDial = (FabSpeedDial) findViewById(R.id.fab_home_dial);
+        fabSpeedDial.setMenuListener(new SimpleMenuListenerAdapter() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public boolean onMenuItemSelected(MenuItem menuItem) {
+                //TODO: Start some activity
+                return false;
             }
         });
     }
