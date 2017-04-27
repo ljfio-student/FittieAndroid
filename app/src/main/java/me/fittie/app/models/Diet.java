6e92808075f63fbaf4bd1ee6c6548f6e2269e82b
Luke Fisher <mail@lukefisher.me>
2017-04-27 04:45:45 +0100

    Add in models for data

---
+++ app/src/main/java/me/fittie/app/models/Diet.java
@@ -0,0 +1,10 @@
+package me.fittie.app.models;
+
+/**
+ * Created by luke on 27/04/2017.
+ */
+
+public class Diet {
+    private String name;
+
+}
---
+++ app/src/main/java/me/fittie/app/models/Workout.java
@@ -0,0 +1,8 @@
+package me.fittie.app.models;
+
+/**
+ * Created by luke on 27/04/2017.
+ */
+
+public class Workout {
+}
