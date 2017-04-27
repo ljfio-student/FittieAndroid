5265a26a10cd18cc023c92e45b3e3025ff3832ea
Luke Fisher <mail@lukefisher.me>
2017-04-27 10:54:55 +0100

    Add in first Response object

---
+++ app/src/main/java/me/fittie/app/network/response/DietResponse.java
@@ -0,0 +1,25 @@
+package me.fittie.app.network.response;
+
+import java.util.ArrayList;
+
+/**
+ * Created by luke on 27/04/2017.
+ */
+
+public class DietResponse {
+    public String name;
+    public int owner;
+
+    public ArrayList<MealDietResponse> meals;
+
+    public class MealDietResponse {
+        public int id;
+        public int day;
+        public int order;
+        public boolean completed;
+    }
+
+    public String toString() {
+        return String.format("Diet '%s' has %d meals", name, meals.size());
+    }
+}
