6b5b6008eeac1adbbb396710a790edeef45df2db
Luke Fisher <mail@lukefisher.me>
2017-04-27 03:15:55 +0100

    Add in Singleton class for getting Volley instance

---
+++ app/src/main/java/me/fittie/app/network/Networker.java
@@ -0,0 +1,42 @@
+package me.fittie.app.network;
+
+/**
+ * Created by luke on 27/04/2017.
+ */
+import android.content.Context;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
+
+public class Networker {
+    private static Networker instance;
+    private RequestQueue requestQueue;
+    private static Context context;
+
+    private Networker(Context context) {
+        Networker.context = context;
+        requestQueue = getRequestQueue();
+    }
+
+    public static synchronized Networker getInstance(Context context) {
+        if (instance == null) {
+            instance = new Networker(context);
+        }
+
+        return instance;
+    }
+
+    public RequestQueue getRequestQueue() {
+        if (requestQueue == null) {
+            requestQueue = Volley.newRequestQueue(context.getApplicationContext());
+        }
+
+        return requestQueue;
+    }
+
+    public <T> void addToRequestQueue(Request<T> req) {
+        getRequestQueue().add(req);
+    }
+}
+
