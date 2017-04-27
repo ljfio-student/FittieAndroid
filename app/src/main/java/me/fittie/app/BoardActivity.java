ad7cf0084565b9dd16e2e90646e72db4b6aaff9d
Luke Fisher <mail@lukefisher.me>
2017-04-27 13:28:24 +0100

    Loop through and get days instead of hard code

--- app/src/main/java/me/fittie/app/BoardActivity.java
+++ app/src/main/java/me/fittie/app/BoardActivity.java
@@ -6,6 +6,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class BoardActivity extends AppCompatActivity {
+    private final int[] dayStrings = {
+            R.string.board_day_monday,
+            R.string.board_day_tuesday,
+            R.string.board_day_wednesday,
+            R.string.board_day_thursday,
+            R.string.board_day_friday,
+            R.string.board_day_saturday,
+            R.string.board_day_sunday
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,13 +34,11 @@ public class BoardActivity extends AppCompatActivity {
     private void setupViewPager(ViewPager viewPager) {
         ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
 
-        adapter.addFragment(new BoardDayFragment(), getString(R.string.board_day_monday).toUpperCase());
-        adapter.addFragment(new BoardDayFragment(), getString(R.string.board_day_tuesday).toUpperCase());
-        adapter.addFragment(new BoardDayFragment(), getString(R.string.board_day_wednesday).toUpperCase());
-        adapter.addFragment(new BoardDayFragment(), getString(R.string.board_day_thursday).toUpperCase());
-        adapter.addFragment(new BoardDayFragment(), getString(R.string.board_day_friday).toUpperCase());
-        adapter.addFragment(new BoardDayFragment(), getString(R.string.board_day_saturday).toUpperCase());
-        adapter.addFragment(new BoardDayFragment(), getString(R.string.board_day_sunday).toUpperCase());
+        // Loop through each day and add to board
+        for (int i = 0; i < 7; i++) {
+            // TODO: Get data
+            adapter.addFragment(new BoardDayFragment(), getString(dayStrings[i]).toUpperCase());
+        }
 
         viewPager.setAdapter(adapter);
     }
