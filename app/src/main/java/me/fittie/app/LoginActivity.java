6bde3954fb20fa56ed2467f1c47c0017652b631f
Luke Fisher <mail@lukefisher.me>
2017-04-27 16:12:41 +0100

    Add code to login a user

--- app/src/main/java/me/fittie/app/LoginActivity.java
+++ app/src/main/java/me/fittie/app/LoginActivity.java
@@ -3,22 +3,22 @@ package me.fittie.app;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
+import android.app.LoaderManager.LoaderCallbacks;
+import android.content.CursorLoader;
+import android.content.Loader;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.provider.ContactsContract;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.app.LoaderManager.LoaderCallbacks;
-
-import android.content.CursorLoader;
-import android.content.Loader;
-import android.database.Cursor;
-import android.net.Uri;
-import android.os.AsyncTask;
-
-import android.os.Build;
-import android.os.Bundle;
-import android.provider.ContactsContract;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -29,10 +29,19 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.android.volley.VolleyError;
+
+import java.io.UnsupportedEncodingException;
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 import java.util.List;
 
-import me.fittie.app.R;
+import me.fittie.app.network.GsonPostOrPutRequest;
+import me.fittie.app.network.NetWorker;
+import me.fittie.app.network.request.UserAuthenticateRequestObject;
+import me.fittie.app.network.response.UserAuthenticatedResponseObject;
 
 import static android.Manifest.permission.READ_CONTACTS;
 
@@ -46,28 +55,21 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
      */
     private static final int REQUEST_READ_CONTACTS = 0;
 
-    /**
-     * A dummy authentication store containing known user names and passwords.
-     * TODO: remove after connecting to a real authentication system.
-     */
-    private static final String[] DUMMY_CREDENTIALS = new String[]{
-            "foo@example.com:hello", "bar@example.com:world"
-    };
-    /**
-     * Keep track of the login task to ensure we can cancel it if requested.
-     */
-    private UserLoginTask mAuthTask = null;
-
     // UI references.
     private AutoCompleteTextView mEmailView;
     private EditText mPasswordView;
     private View mProgressView;
     private View mLoginFormView;
 
+    private SharedPreferences preferences;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        preferences = PreferenceManager.getDefaultSharedPreferences(getBaseContext());
+
         // Set up the login form.
         mEmailView = (AutoCompleteTextView) findViewById(R.id.email);
         populateAutoComplete();
@@ -146,9 +148,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
      * errors are presented and no actual login attempt is made.
      */
     private void attemptLogin() {
-        if (mAuthTask != null) {
-            return;
-        }
+        // TODO: Prevent multiple attempts?
 
         // Reset errors.
         mEmailView.setError(null);
@@ -187,8 +187,53 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             // Show a progress spinner, and kick off a background task to
             // perform the user login attempt.
             showProgress(true);
-            mAuthTask = new UserLoginTask(email, password);
-            mAuthTask.execute((Void) null);
+
+            // Create a SHA-256 hash of the password
+            String hashedPassword = "";
+
+            try {
+                MessageDigest md = MessageDigest.getInstance("SHA-256");
+                md.update(password.getBytes("UTF-8"));
+
+                hashedPassword = String.format("%032X", new BigInteger(1, md.digest()));
+            } catch (UnsupportedEncodingException | NoSuchAlgorithmException ex) {
+                Log.e("UserLoginTask", ex.getMessage());
+            }
+
+            // Get the NetWorker instance
+            NetWorker worker = NetWorker.getInstance(getBaseContext());
+
+            UserAuthenticateRequestObject requestObject =
+                    new UserAuthenticateRequestObject(email, hashedPassword.toLowerCase());
+
+            GsonPostOrPutRequest<UserAuthenticateRequestObject, UserAuthenticatedResponseObject> request = new GsonPostOrPutRequest<>(
+                true, "https://api.fittie.me/login", UserAuthenticateRequestObject.class, UserAuthenticatedResponseObject.class,
+                worker.getDefaultHeaders(), requestObject,
+                (UserAuthenticatedResponseObject response) -> {
+                    Log.i("LoginActivity", response.success ? "success" : "failure");
+
+                    showProgress(false);
+
+                    if (!response.success) {
+                        mPasswordView.setError(getString(R.string.error_incorrect_password));
+                        mPasswordView.requestFocus();
+                    } else {
+                        // Store the Token and User Id
+                        preferences.edit()
+                                .putBoolean("logged_in", response.success)
+                                .putString("user_token", response.token)
+                                .putInt("user_id", response.id)
+                                .commit();
+
+                        // Complete this activity
+                        finish();
+                    }
+
+                }, (VolleyError error) -> {
+                    Log.e("LoginActivity", error.toString());
+                });
+
+            worker.addToRequestQueue(request);
         }
     }
 
@@ -292,61 +337,5 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
         int IS_PRIMARY = 1;
     }
 
-    /**
-     * Represents an asynchronous login/registration task used to authenticate
-     * the user.
-     */
-    public class UserLoginTask extends AsyncTask<Void, Void, Boolean> {
-
-        private final String mEmail;
-        private final String mPassword;
-
-        UserLoginTask(String email, String password) {
-            mEmail = email;
-            mPassword = password;
-        }
-
-        @Override
-        protected Boolean doInBackground(Void... params) {
-            // TODO: attempt authentication against a network service.
-
-            try {
-                // Simulate network access.
-                Thread.sleep(2000);
-            } catch (InterruptedException e) {
-                return false;
-            }
-
-            for (String credential : DUMMY_CREDENTIALS) {
-                String[] pieces = credential.split(":");
-                if (pieces[0].equals(mEmail)) {
-                    // Account exists, return true if the password matches.
-                    return pieces[1].equals(mPassword);
-                }
-            }
-
-            // TODO: register the new account here.
-            return true;
-        }
-
-        @Override
-        protected void onPostExecute(final Boolean success) {
-            mAuthTask = null;
-            showProgress(false);
-
-            if (success) {
-                finish();
-            } else {
-                mPasswordView.setError(getString(R.string.error_incorrect_password));
-                mPasswordView.requestFocus();
-            }
-        }
-
-        @Override
-        protected void onCancelled() {
-            mAuthTask = null;
-            showProgress(false);
-        }
-    }
 }
 
