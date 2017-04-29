28f64b4ca7bffbdc972b92cd14d0cc68bd0ad6ce
Luke Fisher <mail@lukefisher.me>
2017-04-29 06:28:12 +0100

    Update models to include a 'day'

--- app/src/main/java/me/fittie/app/models/Item.java
+++ app/src/main/java/me/fittie/app/models/Item.java
@@ -1,19 +1,33 @@
 package me.fittie.app.models;
 
+import com.j256.ormlite.field.DatabaseField;
+import com.j256.ormlite.table.DatabaseTable;
+
 /**
  * Created by Luke on 28/04/2017.
  */
 
 public class Item {
+    @DatabaseField(id = true)
     private int id;
+
+    @DatabaseField
     private String name;
+
+    @DatabaseField(canBeNull = true)
     private String description;
+
+    private int day;
+
     private int order;
 
-    public Item(int id, String name, String description, int order) {
+    public Item() { }
+
+    public Item(int id, String name, String description, int day, int order) {
         this.id = id;
         this.name = name;
         this.description = description;
+        this.day = day;
         this.order = order;
     }
 
@@ -27,6 +41,10 @@ public class Item {
 
     public int getId() {
         return id;
+    }
+
+    public int getDay() {
+        return day;
     }
 
     public int getOrder() {
--- app/src/main/java/me/fittie/app/models/Meal.java
+++ app/src/main/java/me/fittie/app/models/Meal.java
@@ -1,11 +1,16 @@
 package me.fittie.app.models;
 
+import com.j256.ormlite.table.DatabaseTable;
+
 /**
  * Created by luke on 27/04/2017.
  */
 
+@DatabaseTable(tableName = "meal")
 public class Meal extends Item {
-    public Meal(int id, String name, String description, int order) {
-        super(id, name, description, order);
+    public Meal() { }
+
+    public Meal(int id, String name, String description, int day, int order) {
+        super(id, name, description, day, order);
     }
 }
